687790225cd883cd0712dbc88d0807425e5283d5
burakcanbz <burak.canbaz.web@gmail.com>
2024-09-22 00:21:14 +0300

    feat: signup form with formik added {WIP}

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -8,10 +8,17 @@ import loginGif from '../assets/loginGif.gif'
 const Login = () => {
     const [isChecked, setIsChecked] = useState<boolean>(false);
     const paperStyle = { padding: 30, height: '65vh', width: 400, margin: '50px auto', color: 'white' }
-    const inputLabelStyle = {style: {color: '#fff' }}
+    const inputLabelStyle = { style: { color: '#fff' } }
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setIsChecked(prev => !prev);
     }
+    const style = {
+        '& .MuiInput-underline:before' : {borderBottomColor: 'white'},
+        '& .MuiInput-underline:after' : {borderBottomColor: 'white' },
+        "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+          borderBottomColor: "white"
+        }
+      }
     return (
         <Grid>
             <Paper sx={{
@@ -19,25 +26,26 @@ const Login = () => {
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: 'cover'
             }} style={paperStyle} elevation={5}>
+
                 <Grid container direction='column' rowSpacing={{ xs: 1, sm: 1, md: 1 }}>
                     <Grid display='flex' justifyContent='center' alignItems='center' mt={3}>
-                        <Avatar sx={{ width: 100, height: 100, bgcolor: 'green' }}><LoginIcon sx={{ fontSize: '40px' }} /></Avatar>
+                        <Avatar sx={{ width: 75, height: 75, bgcolor: 'rgb(220 220 220)' }}><LoginIcon sx={{ fontSize: '40px', color: 'black' }} /></Avatar>
                     </Grid>
                     <Grid display='flex' justifyContent='center' alignItems='center'>
-                        <Typography variant='h4' component='h1' m={2} sx={{ fontWeight: 'bold' }}> LOGIN </Typography>
+                        <Typography variant='h5' component='h1' m={2} sx={{ fontWeight: 'bold' }}> LOGIN </Typography>
                     </Grid>
                     <Grid container display='flex' flexDirection='column' rowSpacing={2} >
-                        <TextField label='Username' variant='standard' fullWidth required 
-                        slotProps={{ inputLabel : inputLabelStyle}} />
-                        <TextField label='Password' variant='standard' fullWidth required 
-                        slotProps={{ inputLabel : inputLabelStyle}}/>
+                        <TextField label='Username' variant='standard' fullWidth required
+                            slotProps={{ inputLabel: inputLabelStyle }} sx={style} />
+                        <TextField label='Password' variant='standard' fullWidth required
+                            slotProps={{ inputLabel: inputLabelStyle }} sx={style}/>
                     </Grid>
                     <Grid container mt={2}>
-                        <FormControlLabel label='Remember me' control={<Checkbox color='secondary' checked={isChecked} onChange={handleChange} />} />
+                        <FormControlLabel label='Remember me' control={<Checkbox style={{ color: 'yellow' }} checked={isChecked} onChange={handleChange} />} />
                     </Grid>
-                    <Stack direction='column' marginTop='20px' spacing={3}>
-                        <Button type='submit' color='success' variant='contained' fullWidth sx={{ padding: '10px' }}>
-                            <Typography variant='h6'>LOGIN</Typography>
+                    <Stack direction='column' alignItems='center' marginTop='20px' spacing={3}>
+                        <Button type='submit' color='inherit' variant='contained' size='small' fullWidth={false} sx={{ padding: '10px 50px'}}>
+                            <Typography variant='body1'>LOGIN</Typography>
                         </Button>
                         <Typography display='flex' justifyContent='flex-end' variant='subtitle2'>Don't have an account?<Link sx={{ marginLeft: '4px', color: 'yellow' }} component={RouterLink} underline='none' to='/signup'>Signup</Link></Typography>
                     </Stack>
